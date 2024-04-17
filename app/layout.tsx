83828bd69ee891b7cfc0853c33d5e7bf3378b56e
Atharva Suryawanshi <atharvasuryawanshi30@gmail.com>
2024-04-17 12:39:26 +0530

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { Analytics } from "@vercel/analytics/react"
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import 'react-datepicker/dist/react-datepicker.css'
 import { Toaster } from "@/components/ui/toaster"
@@ -41,6 +41,7 @@ export default function RootLayout({
       >
         <body className={`${inter.className} bg-dark-2`}>
           {children}
+          <Analytics />
           <Toaster />
         </body>
       </ClerkProvider>
